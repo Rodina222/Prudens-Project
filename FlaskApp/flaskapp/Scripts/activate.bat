5b44f095259539a41c2d513559fc824d10b402dc
shroukshata10 <s-shrouk.shata@zewailcity.edu.eg>
2024-05-18 05:31:17 +0300

    update_notifi

--- FlaskApp/flaskapp/Scripts/activate.bat
+++ FlaskApp/flaskapp/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Shrouk Shata\Desktop\SW Projectt\Prudens-Project\flaskapp\flaskapp
+set VIRTUAL_ENV=C:\Users\Shrouk Shata\Desktop\SW Projectt\Prudens-Project-1\FlaskApp\flaskapp
 
 if not defined PROMPT set PROMPT=$P$G
 
